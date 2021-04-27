bbc9fedf83695d06f3fda0d11ec0eada4032a065
Battula <vrbattula@outlook.com>
2021-04-27 10:46:26 -0400

    Fixes namespace issue for document.rb

---
+++ app/models/entities/documents.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Entities
+  # Entities::Documents
+  module Documents
+  end
+end
---
+++ app/models/entities/templates.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Entities
+  # Entities::Templates
+  module Templates
+  end
+end
