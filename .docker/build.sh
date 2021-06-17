b4bd4b7e2d7afca9220c845b24c9199a6e41e86e
Matt Williams <m.a.dubya@gmail.com>
2021-06-17 13:05:44 -0400

    secret key base config

--- .docker/build.sh
+++ .docker/build.sh
@@ -7,6 +7,7 @@ cp .docker/config/mongoid.yml config/
 cp .docker/config/production.rb config/environments/
 cp .docker/config/credentials.yml.enc config/
 cp .docker/config/master.key config/
+cp .docker/config/secrets.yml config/
 
 docker build --build-arg BUNDLER_VERSION_OVERRIDE='2.2.10' \
              --build-arg NODE_MAJOR='12' \
@@ -20,10 +21,11 @@ docker build --build-arg BUNDLER_VERSION_OVERRIDE='2.2.10' \
              --build-arg MITC_HOST="http://mitc" \
              --build-arg MITC_PORT="3001" \
              --build-arg MITC_URL="http://mitc:3001" \
-	     -f .docker/production/Dockerfile --target app -t $2:$1 .
+             -f .docker/production/Dockerfile --target app -t $2:$1 --network="host" .
 docker push $2:$1
 
 mv config/mongoid.yml.tmp config/mongoid.yml
 mv config/environments/production.rb.tmp config/environments/production.rb
 mv config/credentials.yml.enc.tmp config/credentials.yml.enc
 rm config/master.key
+rm config/secrets.yml
