d256baf4e755f1e5b52f1d6b82cb5255403fba92
nisanth <nisanth.yaganti@gmail.com>
2021-06-20 12:39:53 -0400

    updating instant_preview action to post, to handle large requests

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     end
 
     collection do
-      get :instant_preview
+      post :instant_preview
       get :download_notices
       get :fetch_tokens
       get :fetch_placeholders
