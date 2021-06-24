8124a86f295f55419bd9f8c61de4deaf4d8ec501
Matt Williams <m.a.dubya@gmail.com>
2021-06-24 12:52:55 -0400

    secret key base env variable that works and build script change

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,13 +1,13 @@
 # setup docker image config
 cp config/mongoid.yml config/mongoid.yml.tmp
-cp config/environments/production.rb config/environments/production.rb.tmp
+#cp config/environments/production.rb config/environments/production.rb.tmp
 cp config/credentials.yml.enc config/credentials.yml.enc.tmp
 
 cp .docker/config/mongoid.yml config/
-cp .docker/config/production.rb config/environments/
+#cp .docker/config/production.rb config/environments/
 cp .docker/config/credentials.yml.enc config/
 cp .docker/config/master.key config/
-cp .docker/config/secrets.yml config/
+#cp .docker/config/secrets.yml config/
 
 docker build --build-arg BUNDLER_VERSION_OVERRIDE='2.2.10' \
              --build-arg NODE_MAJOR='12' \
@@ -25,7 +25,7 @@ docker build --build-arg BUNDLER_VERSION_OVERRIDE='2.2.10' \
 docker push $2:$1
 
 mv config/mongoid.yml.tmp config/mongoid.yml
-mv config/environments/production.rb.tmp config/environments/production.rb
+#mv config/environments/production.rb.tmp config/environments/production.rb
 mv config/credentials.yml.enc.tmp config/credentials.yml.enc
 rm config/master.key
-rm config/secrets.yml
+#rm config/secrets.yml
