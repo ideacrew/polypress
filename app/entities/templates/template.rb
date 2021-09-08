5c7b1eb464ecf7074468570409cd0493eacc4196
Dan Thomas <dan@ideacrew.com>
2021-09-08 19:18:07 -0400

    WIP adding render_section tag and associated classes Update docker-compose service: mongodb:laatest Disable polypress despendency on rabbmq dependency (temporary) Remove Tag namespace collition in Entities::Tags

--- app/entities/templates/template.rb
+++ app/entities/templates/template.rb
@@ -9,23 +9,23 @@ class Template
   # @!attribute [r] title
   # A human-friendly title for the template
   # @return [String]
-  attribute :title, Types::String
+  attribute :title, Types::String.meta(omittable: false)
 
   # @!attribute [r] description
   # A verbose explanation of the template.
   # @return [String]
-  attribute :description, Types::String
+  attribute :description, Types::String.meta(omittable: true)
 
   # @!attribute [r] sections
   # A collection of standalone content components to parse and render for
   # this template
   # @return [String]
-  attribute :sections, Sections::Section
+  attribute :sections, Sections::Section.meta(omittable: true)
 
   # @!attribute [r] locale
   # Template's written Language
   # @return [String]
-  attribute :locale, Polypress::Types::String.optional.meta(omittable: true)
+  attribute :locale, Polypress::Types::String.optional.meta(omittable: false)
 
   # @!attribute [r] subject
   # Reference to the person/topic that output document discusses/deals with
@@ -41,14 +41,14 @@ class Template
   # @!attribute [r] order
   # The order in which the sections render in the document
   # @return [Array<String>]
-  attribute :order, Types::Array.of(Types::Symbol)
+  attribute :order, Types::Array.of(Types::Symbol).meta(omittable: true)
 
   # @!attribute [r] tags
   # A list of tags for logical grouping of templates
   # @return [Array<Tag>]
-  attribute :tags,
-            Polypress::Types::Array
-              .of(Entities::Tags::Tag)
-              .optional
-              .meta(omittable: true)
+  # attribute :tags,
+  #           Polypress::Types::Array
+  #             .of(Entities::Tags::Tag)
+  #             .optional
+  #             .meta(omittable: true)
 end
