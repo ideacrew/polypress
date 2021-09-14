aceedd47a6012c7e12569e16e0b3d54ef27db444
Raghuram Ganjala <raghuramg83@gmail.com>
2021-09-14 15:55:25 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,64 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
-  devise_for :accounts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  mount Ckeditor::Engine => '/ckeditor'
-  root 'new/templates#index'
+Rails
+  .application
+  .routes
+  .draw do
+    devise_for :accounts
 
-  namespace :new do
-    resources :templates do
-      member do
-        get :preview
-        delete :delete_notice
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    mount Ckeditor::Engine => '/ckeditor'
+    root 'new/templates#index'
+
+    namespace :new do
+      resources :templates do
+        member do
+          get :preview
+          # delete :delete_notice
+        end
+
+        collection do
+          post :instant_preview
+          get :download_notices
+          get :fetch_tokens
+          get :fetch_placeholders
+          get :fetch_recipients
+          post :upload_notices
+        end
       end
 
-      collection do
-        post :instant_preview
-        get :download_notices
-        get :fetch_tokens
-        get :fetch_placeholders
-        get :fetch_recipients
-        post :upload_notices
+      resources :sections do
+        member do
+          get :preview
+          # delete :delete_section
+        end
+
+        collection do
+          post :instant_preview
+          get :download_sections
+
+          # get :fetch_tokens
+          # get :fetch_placeholders
+          # get :fetch_recipients
+          post :upload_sections
+        end
       end
     end
 
-    resources :sections do
-      member do
-        get :preview
-        delete :delete_section
-      end
+    # resources :templates do
+    #   member do
+    #     get :preview
+    #     delete :delete_notice
+    #   end
 
-      collection do
-        post :instant_preview
-        get :download_sections
-        # get :fetch_tokens
-        # get :fetch_placeholders
-        # get :fetch_recipients
-        post :upload_sections
-      end
-    end
+    #   collection do
+    #     post :instant_preview
+    #     get :download_notices
+    #     get :fetch_tokens
+    #     get :fetch_placeholders
+    #     get :fetch_recipients
+    #     # post :delete_notices
+    #     post :upload_notices
+    #   end
+    # end
   end
-
-  # resources :templates do
-  #   member do
-  #     get :preview
-  #     delete :delete_notice
-  #   end
-
-  #   collection do
-  #     post :instant_preview
-  #     get :download_notices
-  #     get :fetch_tokens
-  #     get :fetch_placeholders
-  #     get :fetch_recipients
-  #     # post :delete_notices
-  #     post :upload_notices
-  #   end
-  # end
-end
