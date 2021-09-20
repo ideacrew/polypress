1ea7dea796cbc78956bdc412a900941310b8a0c6
Raghuram Ganjala <raghuramg83@gmail.com>
2021-09-19 23:45:32 -0400

    Added individual notice subscriber

---
+++ app/event_source/subscribers/individual_notices_subscriber.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Subscribers
+  # Subscriber will receive response payload from enroll and creates ENR document
+  class IndividualNoticesSubscriber
+    include EventSource::Logging
+    include ::EventSource::Subscriber[amqp: 'enroll.individual.notices']
+
+    subscribe(
+      :on_enroll_individual_notices
+    ) do |delivery_info, _metadata, response|
+      logger.info "Subscribers::AccountCreatedSubscriber invoked on_enroll_individual_notices with delivery_info: #{delivery_info}, response: #{response}"
+
+      # routing_key = delivery_info[:routing_key]
+      # find notice template by routing key
+      # call operation with template and payload to generate notice document
+      # publish document to a preconfigured publisher
+
+      payload = JSON.parse(response, symbolize_names: true)
+      result =
+        Enrollments::GenerateAndPublishDocuments.new.call(
+          { family_hash: payload, event_key: 'account_created' }
+        )
+
+      if result.success?
+        logger.info "Subscribers::AccountCreatedSubscriber Result: #{result.success} for payload: #{payload}"
+        ack(delivery_info.delivery_tag)
+      else
+        logger.error "Subscribers::AccountCreatedSubscriber Error: #{result.failure} for payload: #{payload}"
+        nack(delivery_info.delivery_tag)
+      end
+    rescue StandardError => e
+      nack(delivery_info.delivery_tag)
+      logger.error "Subscribers::AccountCreatedSubscriber Error: #{e.backtrace} for payload: #{payload}"
+    end
+  end
+end
