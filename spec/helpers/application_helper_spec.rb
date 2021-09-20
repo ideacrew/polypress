719656a55325ad3a15ea6252cb8a4a23cf9cec2e
Battula <vrbattula@outlook.com>
2021-09-20 16:04:10 -0400

    Fixes application helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe ApplicationHelper, type: :helper do
     let(:name) { :notice }
     let(:msg) { 'Bootstrap 5.1 HTML for a dismissable flash message' }
     let(:html5_string) do
-      "<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">Bootstrap 5.1 HTML for a dismissable
-        flash message\\n&lt;button type=&quot;button&quot; class=&quot;btn-close&quot;
-        data-bs-dismiss=&quot;alert&quot; aria-label=&quot;Close&quot;&gt;&lt;/button&gt;</div>"
+      "<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">Bootstrap 5.1 HTML "\
+        "for a dismissable flash message \n&lt;button type=&quot;button&quot; class=&quot;btn-close&quot; "\
+        "data-bs-dismiss=&quot;alert&quot; aria-label=&quot;Close&quot;&gt;&lt;/button&gt;</div>"
     end
     it 'should produce valid HTML5 tags' do
       expect(helper.format_flash(name, msg)).to eq html5_string
