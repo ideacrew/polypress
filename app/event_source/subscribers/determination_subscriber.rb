50d0b2e37de5cb26b20619d84771aafd553fff89
Battula <vrbattula@outlook.com>
2021-10-06 17:27:23 -0400

    Fixes potential double nack issue

--- app/event_source/subscribers/determination_subscriber.rb
+++ app/event_source/subscribers/determination_subscriber.rb
@@ -21,7 +21,6 @@ module Subscribers
         ack(delivery_info.delivery_tag)
         logger.info "Polypress: polypress_eligibility_determination_subscriber_message; acked for #{routing_key}"
       else
-        nack(delivery_info.delivery_tag)
         results
           .map(&:failure)
           .compact
@@ -32,6 +31,7 @@ module Subscribers
             nacked due to:#{errors}; for routing_key: #{routing_key}, payload: #{payload}"
             )
           end
+        nack(delivery_info.delivery_tag)
       end
     rescue StandardError => e
       nack(delivery_info.delivery_tag)
