6e9dc46b3ca5f1b030ac8b2ed9f35552bc592af7
Vishal Kalletla <vishuk1201@gmail.com>
2022-10-12 12:49:11 -0400

    uncommet determination subscriber code

--- app/event_source/subscribers/determination_subscriber.rb
+++ app/event_source/subscribers/determination_subscriber.rb
@@ -11,38 +11,33 @@ module Subscribers
     ) do |delivery_info, _metadata, response|
       routing_key = delivery_info[:routing_key]
       logger.info "Polypress: invoked on_magi_medicaid_mitc_eligibilities with delivery_info: #{delivery_info} routing_key: #{routing_key}"
-
-      # Disabling notice generations for testing optimization of renewals determination
-      # Enable once the testing is done. Do not merge to trunk with disabled operation.
-      logger.info "Polypress: Skipping notice for medicaid gateway renewal determinations with
-                  delivery_info: #{delivery_info} routing_key: #{routing_key}"
-      # payload = JSON.parse(response, symbolize_names: true)
-      # # event_key = routing_key.split('.').last
-      # results =
-      #   MagiMedicaid::GenerateAndPublishEligibilityDocuments.new.call(
-      #     { payload: payload, event_key: routing_key }
-      #   )
-      # if results.all?(&:success)
-      #   logger.info "Polypress: polypress_eligibility_determination_subscriber_message; acked for #{routing_key}"
-      # else
-      #   results
-      #     .map(&:failure)
-      #     .compact
-      #     .each do |result|
-      #       errors =
-      #         if result.is_a?(String)
-      #           result
-      #         elsif result.failure.is_a?(String)
-      #           result.failure
-      #         else
-      #           result.failure.errors.to_h
-      #         end
-      #       logger.error(
-      #         "Polypress: polypress_eligibility_determination_subscriber_error;
-      #       nacked due to:#{errors}; for routing_key: #{routing_key}, payload: #{payload}"
-      #       )
-      #     end
-      # end
+      payload = JSON.parse(response, symbolize_names: true)
+      # event_key = routing_key.split('.').last
+      results =
+        MagiMedicaid::GenerateAndPublishEligibilityDocuments.new.call(
+          { payload: payload, event_key: routing_key }
+        )
+      if results.all?(&:success)
+        logger.info "Polypress: polypress_eligibility_determination_subscriber_message; acked for #{routing_key}"
+      else
+        results
+          .map(&:failure)
+          .compact
+          .each do |result|
+            errors =
+              if result.is_a?(String)
+                result
+              elsif result.failure.is_a?(String)
+                result.failure
+              else
+                result.failure.errors.to_h
+              end
+            logger.error(
+              "Polypress: polypress_eligibility_determination_subscriber_error;
+            nacked due to:#{errors}; for routing_key: #{routing_key}, payload: #{payload}"
+            )
+          end
+      end
       ack(delivery_info.delivery_tag)
     rescue StandardError, SystemStackError => e
       logger.error(
