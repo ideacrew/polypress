567e9e6f32c5974660335d111e2b388e269802c1
Vishal Kalletla <vishuk1201@gmail.com>
2023-01-11 10:02:14 -0500

    setup additional subscribers

---
+++ app/event_source/subscribers/families/notices/tax_forms/initial1095a_notice_generation_subscriber.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Subscribers
+  module Families
+    module Notices
+      module TaxFroms
+        # Subscriber will receive response payload from medicaid gateway and generate documents
+        class Initial1095aNoticeGenerationSubscriber
+          include EventSource::Logging
+          include ::EventSource::Subscriber[amqp: 'edi_gateway.families.notices.tax_forms.initial1095a']
+
+          subscribe(:on_requested) do |delivery_info, _metadata, response|
+
+            routing_key = delivery_info[:routing_key]
+            logger.info "Polypress: invoked Initial1095aNoticeGenerationSubscriber with delivery_info: #{delivery_info} routing_key: #{routing_key}"
+
+            payload = JSON.parse(response, symbolize_names: true)
+            # event_key = routing_key.split('.').last
+
+            result =
+              PolicyTaxHouseholds::GenerateAndPublishTaxDocuments.new.call(
+                { payload: payload, event_key: routing_key }
+              )
+            if result.success?
+              logger.info "Polypress: Catastrophic1095aNoticeGenerationSubscriber; acked for #{routing_key}"
+            else
+              errors = if result.is_a?(String)
+                         result
+                       elsif result.failure.is_a?(String)
+                         result.failure
+                       else
+                         result.failure.errors.to_h
+                       end
+              logger.error(
+                "Polypress: Catastrophic1095aNoticeGenerationSubscriber_error;
+                    nacked due to:#{errors}; for routing_key: #{routing_key}, payload: #{payload}"
+              )
+            end
+            ack(delivery_info.delivery_tag)
+          rescue StandardError, SystemStackError => e
+            logger.error(
+              "Polypress: Initial1095aNoticeGenerationSubscriber_error: nacked due to backtrace:
+                #{e.backtrace}; for routing_key: #{routing_key}, response: #{response}"
+            )
+            ack(delivery_info.delivery_tag)
+          end
+        end
+      end
+    end
+  end
+end
