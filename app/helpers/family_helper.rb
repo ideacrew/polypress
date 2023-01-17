a7b5a54278f89ebc7cdd00e061ca0768471d20b2
Battula <vrbattula@outlook.com>
2023-01-17 17:26:12 -0500

    fix rubocop

--- app/helpers/family_helper.rb
+++ app/helpers/family_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ModuleLength
 # FamilyHelper
 module FamilyHelper
   include FinancialApplicationHelper
@@ -608,3 +609,4 @@ module FamilyHelper
     }
   end
 end
+# rubocop:enable Metrics/ModuleLength
