25fa4d4207c3df62e8223cd5440371251a5d315a
Raghuram <raghuramg83@gmail.com>
2023-01-26 12:36:41 -0500

    rubocop error fix

--- app/models/irs_yearly_pdf_report.rb
+++ app/models/irs_yearly_pdf_report.rb
@@ -232,7 +232,7 @@ class IrsYearlyPdfReport < PdfReport
     end
 
     annual_aptc_amount = annual_premiums[:tax_credit][:cents]
-    return unless (annual_aptc_amount.present? && annual_aptc_amount.to_f > 0)
+    return unless annual_aptc_amount.present? && annual_aptc_amount.to_f > 0
 
     bounding_box([col2, y_pos], :width => 130) do
       text cents_to_dollars(@catastrophic_corrected ? 0.0 : annual_premiums[:slcsp_benchmark_premium][:cents]), :align => :right
