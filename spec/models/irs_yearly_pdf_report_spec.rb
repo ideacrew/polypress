5137c5f000ece7a43f5ed182205d1abb4acfd079
Sai Praveen Gudimetla <saipraveen.gudimetla@gmail.com>
2023-11-16 16:47:37 -0600

    finds 1095A form based on the calendar year and uploads 2023 pdf (#122)
    * upload 2023 tax template pdf and fetch template based on calendar year
    
    * update rubocop metric class length to 300
    
    * add spec
    
    * remove unnecessary instance variable declartion
    
    * fix rubocop

---
+++ spec/models/irs_yearly_pdf_report_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require "#{Rails.root}/spec/shared_examples/enrollments/family_response"
+
+RSpec.describe IrsYearlyPdfReport, type: :model do
+  describe "initialization" do
+    include_context 'family response from enroll'
+
+    let(:title) { '1095A Tax Document' }
+    let(:print_code) { 'IVLCAP' }
+    let(:family_contract) { AcaEntities::Contracts::Families::FamilyContract.new.call(family_hash) }
+    let(:entity) { AcaEntities::Families::Family.new(family_contract.to_h) }
+    let(:insurance_agreement) { entity.to_h[:households][0][:insurance_agreements][0] }
+    let(:insurance_policy) { insurance_agreement[:insurance_policies][0]}
+    let(:tax_household) { insurance_policy[:aptc_csr_tax_households][0] }
+    let(:included_hbx_ids) { tax_household[:covered_individuals].map { |individual| individual[:person][:hbx_id] } }
+    let(:recipient) { entity.to_h[:family_members][0] }
+
+    it "sets correct calendar_year fetch correct irs_document based on calendar_year" do
+      params = { tax_household: tax_household,
+                 recipient: recipient,
+                 insurance_policy: insurance_policy,
+                 insurance_agreement: insurance_agreement,
+                 included_hbx_ids: included_hbx_ids }
+      irs_yearly_pdf_report = IrsYearlyPdfReport.new(params)
+      reporting_year = insurance_agreement[:plan_year].to_i
+      expect(irs_yearly_pdf_report.instance_variable_get(:@reporting_year)).to eq(reporting_year)
+      expect(irs_yearly_pdf_report.instance_variable_get(:@calender_year)).to eq(reporting_year)
+      expect(irs_yearly_pdf_report.fetch_irs_form_template).to eq("#{Rails.root}/lib/pdf_templates/#{reporting_year}_1095A_form.pdf")
+    end
+
+    it "tax form should exist" do
+      params = { tax_household: tax_household,
+                 recipient: recipient,
+                 insurance_policy: insurance_policy,
+                 insurance_agreement: insurance_agreement,
+                 included_hbx_ids: included_hbx_ids }
+      IrsYearlyPdfReport.new(params)
+      reporting_year = insurance_agreement[:plan_year].to_i
+      expect(File.exist?("#{Rails.root}/lib/pdf_templates/#{reporting_year}_1095A_form.pdf")).to eq true
+    end
+  end
+end
